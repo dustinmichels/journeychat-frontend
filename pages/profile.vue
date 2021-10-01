989be8a1a77682a67ac5b42adf5719a76df7b1dc
Dustin Michels <dustin7538@gmail.com>
2021-10-01 14:27:23 -0600

    add profile pic

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,15 +2,27 @@
   <section class="section">
     <div class="container">
       <h2 class="title">My Profile</h2>
-      <div class="content">
-        <p>
-          <strong>Username:</strong>
-          {{ loggedInUser.username }}
-        </p>
-        <p>
-          <strong>Email:</strong>
-          {{ loggedInUser.email }}
-        </p>
+
+      <div class="columns">
+        <div class="column is-one-quarter">
+          <b-image
+            is-128x128
+            :src="loggedInUser.avatar"
+            alt="Profile Pic"
+            rounded="rounded"
+          >
+          </b-image>
+        </div>
+        <div class="column">
+          <p>
+            <strong>Username:</strong>
+            {{ loggedInUser.username }}
+          </p>
+          <p>
+            <strong>Email:</strong>
+            {{ loggedInUser.email }}
+          </p>
+        </div>
       </div>
     </div>
   </section>
